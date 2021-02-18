f7dc5c956fbb7c7cf7ab535115abcdcfc76ed462
theemutvcode <55019667+theemutvcode@users.noreply.github.com>
2021-02-18 15:15:37 +0100

    add german translation (#4)

---
+++ translations/de_DE.ts
@@ -0,0 +1,76 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE" sourcelanguage="de_DE">
+<context>
+    <name>BrowserMenu</name>
+    <message>
+        <location filename="../qml/BrowserMenu.qml" line="14"/>
+        <source>New Folder</source>
+        <translation>Neuer Ordner</translation>
+    </message>
+    <message>
+        <location filename="../qml/BrowserMenu.qml" line="24"/>
+        <source>Paste</source>
+        <translation>Einfügen</translation>
+    </message>
+    <message>
+        <location filename="../qml/BrowserMenu.qml" line="31"/>
+        <source>Empty Trash</source>
+        <translation>Papierkorb leeren</translation>
+    </message>
+</context>
+<context>
+    <name>BrowserView</name>
+    <message>
+        <location filename="../qml/BrowserView.qml" line="47"/>
+        <source>No Files</source>
+        <translation>Keine Dateien</translation>
+    </message>
+</context>
+<context>
+    <name>ItemMenu</name>
+    <message>
+        <location filename="../qml/ItemMenu.qml" line="23"/>
+        <source>Open</source>
+        <translation>Öffnen</translation>
+    </message>
+    <message>
+        <location filename="../qml/ItemMenu.qml" line="31"/>
+        <source>Copy</source>
+        <translation>Kopieren</translation>
+    </message>
+    <message>
+        <location filename="../qml/ItemMenu.qml" line="39"/>
+        <source>Cut</source>
+        <translation>Ausschneiden</translation>
+    </message>
+    <message>
+        <location filename="../qml/ItemMenu.qml" line="47"/>
+        <source>Remove</source>
+        <translation>Löschen</translation>
+    </message>
+    <message>
+        <location filename="../qml/ItemMenu.qml" line="57"/>
+        <source>Rename</source>
+        <translation>Umbenennen</translation>
+    </message>
+    <message>
+        <location filename="../qml/ItemMenu.qml" line="66"/>
+        <source>Set As Wallpaper</source>
+        <translation>Als Hintergrundbild benutzen</translation>
+    </message>
+    <message>
+        <location filename="../qml/ItemMenu.qml" line="76"/>
+        <source>Properties</source>
+        <translation>Eigenschaften</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../qml/main.qml" line="17"/>
+        <source>File Manager</source>
+        <translation>Dateimanager</translation>
+    </message>
+</context>
+</TS>
