bed789d95e8f3648cfe018fa94124706bd13f273
Anonymous <noreply@weblate.org>
2021-05-05 07:37:03 +0000

    Translated using Weblate (Malayalam)
    Currently translated at 0.0% (0 of 12 strings)
    
    Translation: CyberOS/File Manager
    Translate-URL: https://hosted.weblate.org/projects/cyberos/file-manager/ml/

--- translations/ml_IN.ts
+++ translations/ml_IN.ts
@@ -6,17 +6,17 @@
     <message>
         <location filename="../qml/BrowserMenu.qml" line="14"/>
         <source>New Folder</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">പുതിയ ഫോൾഡർ</translation>
     </message>
     <message>
         <location filename="../qml/BrowserMenu.qml" line="24"/>
         <source>Paste</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">പേസ്റ്റ്</translation>
     </message>
     <message>
         <location filename="../qml/BrowserMenu.qml" line="31"/>
         <source>Empty Trash</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">ശൂന്യമായ ട്രാഷ്</translation>
     </message>
 </context>
 <context>
@@ -24,7 +24,7 @@
     <message>
         <location filename="../qml/BrowserView.qml" line="47"/>
         <source>No Files</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">ഫയലുകളൊന്നുമില്ല</translation>
     </message>
 </context>
 <context>
@@ -32,37 +32,37 @@
     <message>
         <location filename="../qml/ItemMenu.qml" line="23"/>
         <source>Open</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">തുറക്കുക</translation>
     </message>
     <message>
         <location filename="../qml/ItemMenu.qml" line="31"/>
         <source>Copy</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">പകർത്തുക</translation>
     </message>
     <message>
         <location filename="../qml/ItemMenu.qml" line="39"/>
         <source>Cut</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">മുറിക്കുക</translation>
     </message>
     <message>
         <location filename="../qml/ItemMenu.qml" line="47"/>
         <source>Remove</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">നീക്കംചെയ്യുക</translation>
     </message>
     <message>
         <location filename="../qml/ItemMenu.qml" line="57"/>
         <source>Rename</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">പേരുമാറ്റുക</translation>
     </message>
     <message>
         <location filename="../qml/ItemMenu.qml" line="66"/>
         <source>Set As Wallpaper</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">വാൾപേപ്പറായി സജ്ജമാക്കുക</translation>
     </message>
     <message>
         <location filename="../qml/ItemMenu.qml" line="76"/>
         <source>Properties</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">പ്രോപ്പർട്ടികൾ</translation>
     </message>
 </context>
 <context>
@@ -70,7 +70,7 @@
     <message>
         <location filename="../qml/main.qml" line="17"/>
         <source>File Manager</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">ഫയൽ മാനേജർ</translation>
     </message>
 </context>
 </TS>
