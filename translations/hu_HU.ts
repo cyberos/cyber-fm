86d4778bccedac262bbf7ed8435902ee45d82290
Weblate <noreply@weblate.org>
2021-05-11 16:51:10 +0200

    Added translation using Weblate (Hungarian)

---
+++ translations/hu_HU.ts
@@ -0,0 +1,76 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hu" sourcelanguage="de_DE">
+<context>
+    <name>BrowserMenu</name>
+    <message>
+        <location filename="../qml/BrowserMenu.qml" line="14"/>
+        <source>New Folder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/BrowserMenu.qml" line="24"/>
+        <source>Paste</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/BrowserMenu.qml" line="31"/>
+        <source>Empty Trash</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>BrowserView</name>
+    <message>
+        <location filename="../qml/BrowserView.qml" line="47"/>
+        <source>No Files</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ItemMenu</name>
+    <message>
+        <location filename="../qml/ItemMenu.qml" line="23"/>
+        <source>Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/ItemMenu.qml" line="31"/>
+        <source>Copy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/ItemMenu.qml" line="39"/>
+        <source>Cut</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/ItemMenu.qml" line="47"/>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/ItemMenu.qml" line="57"/>
+        <source>Rename</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/ItemMenu.qml" line="66"/>
+        <source>Set As Wallpaper</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/ItemMenu.qml" line="76"/>
+        <source>Properties</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../qml/main.qml" line="17"/>
+        <source>File Manager</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
