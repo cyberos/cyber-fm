f5bb7f55817107d829e90461159762d473a2303b
Anonymous <noreply@weblate.org>
2021-05-18 22:25:56 +0000

    Translated using Weblate (Yue (yue_HK))
    Currently translated at 0.0% (0 of 12 strings)
    
    Translation: CyberOS/File Manager
    Translate-URL: https://hosted.weblate.org/projects/cyberos/file-manager/yue_HK/

--- translations/yue_HK.ts
+++ translations/yue_HK.ts
@@ -6,17 +6,17 @@
     <message>
         <location filename="../qml/BrowserMenu.qml" line="14"/>
         <source>New Folder</source>
-        <translation>新文件夾</translation>
+        <translation type="unfinished">新文件夾</translation>
     </message>
     <message>
         <location filename="../qml/BrowserMenu.qml" line="24"/>
         <source>Paste</source>
-        <translation>貼上</translation>
+        <translation type="unfinished">貼上</translation>
     </message>
     <message>
         <location filename="../qml/BrowserMenu.qml" line="31"/>
         <source>Empty Trash</source>
-        <translation>清空垃圾桶</translation>
+        <translation type="unfinished">清空垃圾桶</translation>
     </message>
 </context>
 <context>
@@ -24,7 +24,7 @@
     <message>
         <location filename="../qml/BrowserView.qml" line="47"/>
         <source>No Files</source>
-        <translation>冇任何文件</translation>
+        <translation type="unfinished">冇任何文件</translation>
     </message>
 </context>
 <context>
@@ -32,37 +32,37 @@
     <message>
         <location filename="../qml/ItemMenu.qml" line="23"/>
         <source>Open</source>
-        <translation>開啟</translation>
+        <translation type="unfinished">開啟</translation>
     </message>
     <message>
         <location filename="../qml/ItemMenu.qml" line="31"/>
         <source>Copy</source>
-        <translation>複製</translation>
+        <translation type="unfinished">複製</translation>
     </message>
     <message>
         <location filename="../qml/ItemMenu.qml" line="39"/>
         <source>Cut</source>
-        <translation>剪低</translation>
+        <translation type="unfinished">剪低</translation>
     </message>
     <message>
         <location filename="../qml/ItemMenu.qml" line="47"/>
         <source>Remove</source>
-        <translation>移除</translation>
+        <translation type="unfinished">移除</translation>
     </message>
     <message>
         <location filename="../qml/ItemMenu.qml" line="57"/>
         <source>Rename</source>
-        <translation>重新命名</translation>
+        <translation type="unfinished">重新命名</translation>
     </message>
     <message>
         <location filename="../qml/ItemMenu.qml" line="66"/>
         <source>Set As Wallpaper</source>
-        <translation>設為壁布</translation>
+        <translation type="unfinished">設為壁布</translation>
     </message>
     <message>
         <location filename="../qml/ItemMenu.qml" line="76"/>
         <source>Properties</source>
-        <translation>屬性</translation>
+        <translation type="unfinished">屬性</translation>
     </message>
 </context>
 <context>
@@ -70,7 +70,7 @@
     <message>
         <location filename="../qml/main.qml" line="17"/>
         <source>File Manager</source>
-        <translation>檔案管理員</translation>
+        <translation type="unfinished">檔案管理員</translation>
     </message>
 </context>
 </TS>
